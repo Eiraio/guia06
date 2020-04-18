e931b70a9593ced9e1016c6bb5c0ce791e65ea47
Eira Micaela Martinez <eiramicaelamartinez@gmail.com>
2020-04-18 18:27:34 -0300

    Resolví el paso 02

--- src/died/guia06/Alumno.java
+++ src/died/guia06/Alumno.java
@@ -3,12 +3,16 @@ package died.guia06;
 import java.util.List;
 
 
-public class Alumno {
+public class Alumno extends Comparable<Alumno>{
 
 	private String nombre;
 	private Integer nroLibreta;
 	private List<Curso> cursando;
 	private List<Curso> aprobados;
+
+	public String getNombre() {
+		return nombre;
+	}
 
 	public int creditosObtenidos() {
 		return 1;
@@ -20,6 +24,16 @@ public class Alumno {
 
 	public void inscripcionAceptada(Curso c) {
 		//
+	}
+
+	public Boolean equals(Alumno alumno){
+		if(this.nroLibreta == alumno.nroLibreta) return true;
+		return false;
+	}
+
+	@Override
+	public int compareTo(Alumno a){
+		return (int) (this.nombre.compareTo(a.getNombre()));
 	}
 
 
